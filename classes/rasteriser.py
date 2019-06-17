63a7bc545f41457b234faf0d659a4f79f1465bb6
davidherbert2 <david.herbert2@newcastle.ac.uk>
2019-06-17 14:59:13 +0100

    Rasteriser moved to class, develop unit tests

--- classes/rasteriser.py
+++ classes/rasteriser.py
@@ -58,7 +58,7 @@ class Rasteriser:
         }
     }
         
-     def __init__(self, 
+    def __init__(self, 
                   geojson_data,                           # Extracted GeoJSON data
                   area_codes = 'all',                     # Boundary specified either by area codes
                   bounding_box = None,                    # Bounding box as a list [xmin, ymin, xmax, ymax]
@@ -100,36 +100,45 @@ class Rasteriser:
             
         # Validate arguments against the schema
         v = Validator()
-        args_ok = v.validate(args, ARG_SCHEMA)
+        args_ok = v.validate(args, self.ARG_SCHEMA)
         if (args_ok):
             # Validated successfully
             self.logger.info('Argument validation passed')
             self.logger.info(', '.join('%s: %s' % arg for arg in args.items()))
+            self.geojson_data    = geojson_data
+            self.area_codes      = area_codes
+            self.bounding_box    = bounding_box
+            self.scale           = scale
+            self.output_filename = output_filename
+            self.output_format   = output_format
+            self.resolution      = resolution
+            self.area_threshold  = area_threshold
+            self.invert          = invert
         else:
             # Validation fails, log errors
             self.logger.warning('Argument validation failed, errors follow:')
             for name, errmsg in v.errors:
                 self.logger.warning('--- {} returned "{}"'.format(name, errmsg))
         
-       def create(self):
-           """
-           Generate the output raster dataset
-           """
-           try:
+    def create(self):
+        """
+        Generate the output raster dataset
+        """
+        try:
             # Read the supplied GeoJSON data into a DataFrame
             self.logger.info('Creating GeoDataFrame from input...')
-            input_data = geopandas.GeoDataFrame(geojson_data)
+            input_data = geopandas.GeoDataFrame(self.geojson_data)
             self.logger.info('Done')
             
             # Create the fishnet
-            if bounding_box is not None:
+            if self.bounding_box is not None:
                 # Use the supplied British National Grid bounding box
                 self.logger.info('Generate fishnet GeoDataFrame from supplied bounding box...')
-                fishnet_geojson = FishNet(bbox=bounding_box, netsize=resolution).create()   
+                fishnet_geojson = FishNet(bbox=self.bounding_box, netsize=self.resolution).create()   
             else:
                 # Use the LAD codes
                 self.logger.info('Generate fishnet GeoDataFrame from supplied bounding box...')
-                fishnet_geojson = FishNet(lad=area_codes, netsize=resolution).create()
+                fishnet_geojson = FishNet(lad=self.area_codes, netsize=self.resolution).create()
             fishnet = geopandas.GeoDataFrame(fishnet_geojson)
             self.logger.info('Done')
             
@@ -137,7 +146,7 @@ class Rasteriser:
             self.logger.info('Overlay data on fishnet using intersection...')
             intersection = geopandas.overlay(fishnet, input_data, how='intersection')
             self.logger.info('Done')
-            
+        
             # Write area attribute into frame
             self.logger.info('Computing areas...')
             intersection['area'] = intersection.geometry.area
@@ -147,16 +156,16 @@ class Rasteriser:
             self.logger.info('Doing merge...')
             int_merge = fishnet.merge(intersection.groupby(['ID']).area.sum()/100.0, on='ID')
             for i, row in int_merge.iterrows():
-                if row['area'] > area_threshold:
-                    int_merge.at[i, 'include_me'] = int(0) if invert else int(1)
+                if row['area'] > self.area_threshold:
+                    int_merge.at[i, 'include_me'] = int(0) if self.invert else int(1)
                 else:
-                    int_merge.at[i, 'include_me'] = int(1) if invert else int(0)
+                    int_merge.at[i, 'include_me'] = int(1) if self.invert else int(0)
             self.logger.info('Done')        
             
             self.logger.info('Compute bounds of dataset...')
             x_min, y_min, x_max, y_max = int_merge.total_bounds
-            xdim = int((x_max - x_min) / resolution)
-            ydim = int((y_max - y_min) / resolution)
+            xdim = int((x_max - x_min) / self.resolution)
+            ydim = int((y_max - y_min) / self.resolution)
             self.logger.info('xmin = {}, ymin = {}, xmax = {}, ymax = {}'.format(x_min, y_min, x_max, y_max))
             
             # Save as temporary shapefile (TO DO - understand what information is gained by doing this that is not present in GeoJSON)
@@ -167,12 +176,12 @@ class Rasteriser:
             
             # Open OGR dataset
             ogr_source = ogr.Open(temp_shp)
-            output_file = '{}/output_raster.tif'.format(Config.get('DATA_DIRECTORY'), output_filename)
+            output_file = '{}/output_raster.tif'.format(Config.get('DATA_DIRECTORY'),self. output_filename)
             self.logger.info('Will write output raster to {}'.format(output_file))
             
             driver = gdal.GetDriverByName('GTiff')
             rasterised = driver.Create(output_file, xdim, ydim, 1, gdal.GDT_Byte)
-            rasterised.SetGeoTransform((x_min, resolution, 0, y_max, 0, -resolution))
+            rasterised.SetGeoTransform((x_min, self.resolution, 0, y_max, 0, -self.resolution))
             srs = osr.SpatialReference()
             srs.ImportFromEPSG(27700)
             rasterised.SetProjection(srs.ExportToWkt())
